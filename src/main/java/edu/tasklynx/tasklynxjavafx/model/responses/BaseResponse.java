6ef97ce0aec11acc5a1555b22563c76c46941571
Miguel <ayorino23@hotmail.com>
2024-05-09 19:42:31 +0200

    Updating BaseResponse

--- src/main/java/edu/tasklynx/tasklynxjavafx/model/responses/BaseResponse.java
+++ src/main/java/edu/tasklynx/tasklynxjavafx/model/responses/BaseResponse.java
@@ -1,8 +1,11 @@
 package edu.tasklynx.tasklynxjavafx.model.responses;
 
+import java.util.List;
+
 public class BaseResponse {
     protected boolean error;
     protected String errorMessage;
+    protected List<String> errorsList;
 
     public boolean isError() {
         return error;
@@ -11,4 +14,6 @@ public class BaseResponse {
     public String getErrorMessage() {
         return errorMessage;
     }
+
+    public List<String> getErrorsList() { return errorsList; }
 }

4cf842fbb23524c68e0d93dc2ddfb9dac78a6e87
DanielAlmazan <almazansellesdaniel@gmail.com>
2024-05-24 00:11:17 +0200

    - Added two new constants for Hotel Nest connection - Added Habitacion and HabitacionResponse for getting rooms responses - Added dataSources to .gitignore

---
+++ src/main/java/edu/tasklynx/tasklynxjavafx/model/Habitacion.java
@@ -0,0 +1,27 @@
+package edu.tasklynx.tasklynxjavafx.model;
+
+public class Habitacion {
+    private String _id;
+    private int numero;
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return "" + numero;
+    }
+}
